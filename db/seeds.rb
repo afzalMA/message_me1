c26b8baaa0b4ac532491a4cd95d98275a08d8a3d
afzalMa <afzal.m@etvbharat.com>
2020-09-02 17:51:24 +0530

    created user and messages models and tables

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.create(username: "Afzal", password: "password")
+User.create(username: "mohammed", password: "password")
+User.create(username: "sohail", password: "password")
+User.create(username: "azhar", password: "password")
+User.create(username: "akram", password: "password")
+
+
+Message.create(body: "sample message", user: User.first)
+Message.create(body: "sample message", user: User.find(3))
+Message.create(body: "sample message", user: User.find(4))
+
+
